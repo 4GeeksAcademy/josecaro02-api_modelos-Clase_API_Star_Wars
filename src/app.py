7b812dac9135261efe2ada6eb7f6357f4a194607
Jose David Caro Cantor <43614556+josecaro02@users.noreply.github.com>
2024-09-14 00:25:34 +0000

    [Minor changes]

--- src/app.py
+++ src/app.py
@@ -40,7 +40,7 @@ def sitemap():
 def handle_hello():
 
     response_body = {
-        "msg": "Hello, this is your GET /user response! "
+        "msg": "Hello, this is your GET /user response!!!!!"
     }
 
     return jsonify(response_body), 200
